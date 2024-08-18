b1e8482a13bf6380dc53d67fc70f0e0060307965
SC <70444245+SC136@users.noreply.github.com>
2024-08-18 14:09:07 +0530

    Update receiver.ino

--- Codes/receiver.ino
+++ Codes/receiver.ino
@@ -10,7 +10,7 @@ RF24 radio(7, 8); // CE, CSN
 const byte address[6] = "00001";
 
 // motor control
-const int motorPin = 3;
+Servo esc;
 int throttleValue = 0;
 
 // servo control
@@ -32,11 +32,15 @@ void setup() {
   radio.setPALevel(RF24_PA_MIN);
   radio.startListening();
 
-  pinMode(motorPin, OUTPUT);
+  esc.attach(3);
 
   // attach servos to pins
   servoX.attach(9);
   servoY.attach(10);
+
+  // initialize the ESC (minimum throttle)
+  esc.writeMicroseconds(1000);
+  delay(2000); // wait for 2 seconds for the ESC to initialize
 }
 
 void loop() {
@@ -44,8 +48,8 @@ void loop() {
     radio.read(&data, sizeof(Data_Package));
 
     // map and control the motor speed (throttle)
-    throttleValue = map(data.throttle, 0, 1023, 0, 255);
-    analogWrite(motorPin, throttleValue);
+    throttleValue = map(data.throttle, 0, 1023, 1000, 2000);
+    esc.writeMicroseconds(throttleValue);
 
     // map and control the servos
     int servoXVal = map(data.servoX, 0, 1023, 0, 180);
