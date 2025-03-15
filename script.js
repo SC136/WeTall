fdd0c300de9b7feb8b4268e7fa49e9f0bc946e8c
Sahil Karpe <167101357+ivory-26@users.noreply.github.com>
2025-03-15 16:50:55 +0530

    syntax error 😭 in js

--- script.js
+++ script.js
@@ -6,7 +6,7 @@ function showSlides(n) {
     if (n >= slides.length) { slideIndex = 0 }
     if (n < 0) { slideIndex = slides.length - 1 }
     for (let i = 0; i < slides.length; i++) {
-        slides[i],classList.remove("active");
+        slides[i].classList.remove("active");
     }
     slides[slideIndex].classList.add("active");
 }
