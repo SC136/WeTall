aad38ff016fd01141a746d8dbbc4858966fe1ac1
Sahil Karpe <167101357+ivory-26@users.noreply.github.com>
2025-03-15 16:37:11 +0530

    carousel images test again

--- script.js
+++ script.js
@@ -6,9 +6,9 @@ function showSlides(n) {
     if (n >= slides.length) { slideIndex = 0 }
     if (n < 0) { slideIndex = slides.length - 1 }
     for (let i = 0; i < slides.length; i++) {
-        slides[i].style.display = "none";
+        slides[i],classList.remove("active");
     }
-    slides[slideIndex].style.display = "block";
+    slides[slideIndex].classList.add("active");
 }
 
 document.querySelector(".prev").addEventListener("click", () => {
